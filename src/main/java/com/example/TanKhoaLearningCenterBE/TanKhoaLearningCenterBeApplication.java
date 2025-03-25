199fc647256e9b4a258fc1f8cc50e46d4c2e14fc
HarryNguyen6678 <103439893+HarryNguyen6678@users.noreply.github.com>
2025-03-25 11:16:49 +0700

    first commit

---
+++ src/main/java/com/example/TanKhoaLearningCenterBE/TanKhoaLearningCenterBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TanKhoaLearningCenterBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanKhoaLearningCenterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanKhoaLearningCenterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TanKhoaLearningCenterBE/TanKhoaLearningCenterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TanKhoaLearningCenterBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanKhoaLearningCenterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
