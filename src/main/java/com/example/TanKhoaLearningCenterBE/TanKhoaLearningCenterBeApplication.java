36d964dfe04ea94fb7ff3707e8d4d95876574160
HarryNguyen6678 <103439893+HarryNguyen6678@users.noreply.github.com>
2025-03-25 14:21:16 +0700

    Modify Maven and Java Version

--- src/main/java/com/example/TanKhoaLearningCenterBE/TanKhoaLearningCenterBeApplication.java
+++ src/main/java/com/example/TanKhoaLearningCenterBE/TanKhoaLearningCenterBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TanKhoaLearningCenterBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TanKhoaLearningCenterBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TanKhoaLearningCenterBeApplication.class, args);
+    }
 
 }
